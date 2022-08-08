7993b845526d90b8613d8dab13decd5c5e3cbcb2
Asier Aguado <asier.aguado@cern.ch>
2022-08-08 17:45:29 +0200

    Initial commit

---
+++ bashrc-functions.sh
@@ -0,0 +1,33 @@
+git_current_branch () {
+    local ref
+    ref=$(command git symbolic-ref --quiet HEAD 2> /dev/null)
+    local ret=$?
+    if [[ $ret != 0 ]]
+    then
+        [[ $ret == 128 ]] && return
+        ref=$(command git rev-parse --short HEAD 2> /dev/null)  || return
+    fi
+    echo ${ref#refs/heads/}
+}
+
+ldapuser() {
+    ldapsearch -H ldap://xldap.cern.ch -x -b "OU=Users,OU=Organic Units,DC=cern,DC=ch" "CN=$1"
+}
+
+ldaplw() {
+    ldapsearch -H ldap://xldap.cern.ch -x -b "OU=Externals,DC=cern,DC=ch" "mail=$1"
+}
+
+ldapgroup() {
+    ldapsearch -H ldap://xldap.cern.ch -x -b "OU=Workgroups,DC=cern,DC=ch" "CN=$1"
+}
+
+sshdel() {
+    if  [ -z "$1" ]
+    then
+	   echo "Usage: sshdel [host]"
+    else 
+           sed -i /$1/d ~/.ssh/known_hosts
+    fi
+}
+
